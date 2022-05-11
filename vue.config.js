e84384a684f6f901a1617591af7052d3bc2f29d4
cooper <tuajetaime@gmail.com>
2022-05-11 17:00:37 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-const currentTime = new Date().getTime()
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: currentTime + '_static',
 })
