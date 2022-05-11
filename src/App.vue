63eb226787c58688e1cfdf223f3302ac477ad436
cooper <tuajetaime@gmail.com>
2022-05-11 10:14:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "PingFang SC";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
